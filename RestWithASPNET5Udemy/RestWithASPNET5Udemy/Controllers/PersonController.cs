3258408a7c965f15a15f30f2af6e48b43d1d750b
Lucas Lorenção <llorencao@hotmail.com>
2022-05-22 20:23:53 -0300

    Implementando Get, Post, Put e Delete

--- RestWithASPNET5Udemy/RestWithASPNET5Udemy/Controllers/PersonController.cs
+++ RestWithASPNET5Udemy/RestWithASPNET5Udemy/Controllers/PersonController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestWithASPNET5Udemy.Model;
+using RestWithASPNET5Udemy.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,19 +16,49 @@ namespace RestWithASPNET5Udemy.Controllers
         
 
         private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
 
-        public PersonController(ILogger<PersonController> logger)
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
             _logger = logger;
+            _personService = personService;
         }
 
-        [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Sum(string firstNumber, string secondNumber)
+        [HttpGet]
+        public IActionResult Get()
         { 
 
-            return BadRequest("Invalid Input");
+            return Ok(_personService.FindAll());
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindByID(id);
+            if (person == null) return NotFound();
+            return Ok(person);
         }
 
+        [HttpPost("{id}")]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+        
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
 
 
 
@@ -127,10 +159,10 @@ namespace RestWithASPNET5Udemy.Controllers
         //    {
         //        return decimalValue;
         //    }
-            
+
         //    return 0;
         //}
 
-        
+
     }
 }
