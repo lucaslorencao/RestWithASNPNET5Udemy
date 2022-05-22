61f4b3e7b85ec74b66bb39b8f98ea3f7c089d972
Lucas Lorenção <llorencao@hotmail.com>
2022-05-22 19:15:03 -0300

    Implementando Service

---
+++ RestWithASPNET5Udemy/RestWithASPNET5Udemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNET5Udemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5Udemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
